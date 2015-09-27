5a6b158dbb832e39bbc8c8c42f709784494eaaf9
vaibhav walia <walia.vaibhav@yahoo.com>
2015-09-27 18:49:41 +0000

    Only upload working

--- CloudPhotoStorageService/requestHandlers.js
+++ CloudPhotoStorageService/requestHandlers.js
@@ -32,17 +32,18 @@ function start(request,response,pictures){
 function upload(request,response,pictures){
  var url = require("url");
  var querystring = require('querystring');
- var query = url.parse(request.url).query;
- var album = querystring.parse(response.url)["album"];
+ var query = url.parse(request.url,true).query;
+ var album = query["album"];
  if(!album) album = 'album1';
  var form = new formidable.IncomingForm();
  form.parse(request,function(error,fields,files){
+   console.log(request);
    if(error) throw error;
-    if(files.upload.size){
+    if(files.upload){
     var path = files.upload.path;
     var filename = files.upload.name;
     fs.readFile(path,function(err,data){
-     if(err) throw err;
+     if(err){ response.writeHead(500,{"Content-Type":"application/json"}); throw(err); };
      var base64data = new Buffer(data).toString('base64');
      pictures.find({album : album}).toArray(function(err,data){
      console.log("Error : "+err);
@@ -54,43 +55,33 @@ function upload(request,response,pictures){
                     };
        console.log(toInsert);
        pictures.insert(toInsert,function(err,data){
-       response.writeHead({"Content-Type":"text/html"});
-       response.end("<html><body><b> Data Received</b> <form action='/show'><input type ='submit' value='Ok' /></form> </body></html>");
+       response.writeHead(201,{"Content-Type":"application/json"});
+       var toret = "{ \"album\" :\""+album+"\", \"filename\" : \""+filename+"\", \"url\" : \"/show?album='"+album+"'&filename='"+filename+"'\"}";
+       response.end(toret);
       });
      }
      else{
          console.log(data[0]);
+         console.log("here");
          var pics = data[0].pictures;
          pics.push({base64Img : base64data,filename : filename});
          pictures.update({ album : album },{album:album,pictures:pics},function(err,data){
-         if(err) throw err;
+         if(err){response.writeHead(500,{"Content-Type":"application/json"}); throw(err);  };
          console.log(data);
-         response.writeHead({"Content-Type":"text/html"});
-         response.end("<html><body><b> Data Received</b> <form action='/show'><input type ='submit' value='Ok' /></form> </body></html>");
+         response.writeHead(201,{"Content-Type":"application/json"});
+         var toret = "{ \"album\" :\""+album+"\", \"filename\" : \""+filename+"\", \"url\" : \"/show?album='"+album+"'&filename='"+filename+"'\"}";
+         response.end(toret);
          });
     }  
     });
-    /*
-     var toInsert = {
-                     album : album,
-                     pictures : [],
-                     Base64Img : base64data,
-                     filename : filename
-                    };
-     console.log(toInsert);
-     pictures.insert(toInsert,function(err,data){
-      response.writeHead({"Content-Type":"text/html"});
-      response.end("<html><body><b> Data Received</b> <form action='/show'><input type ='submit' value='Ok' /></form> </body></html>");     
-     });*/
     });
    }
    else{
-    response.writeHead({"Content-Type":"text/html"});
-    response.end("<html><body><b> No Data Received</b> <form action='/'><input type ='submit' value='Ok' /></form> </body></html>");
+    console.log(files);
+    response.writeHead(400,{"Content-Type":"application/json"});
+    response.end();
    }
- });
-
-      
+ });    
 }
 
 function remove(request,response,pictures){
@@ -100,27 +91,49 @@ function remove(request,response,pictures){
 }
 
 function show(request,response,pictures){
+
  var url = require("url");
  var querystring = require('querystring');
- var query = url.parse(request.url).query;
- var album = querystring.parse(response.url)["album"];
- //var filename = querystring.parse(i
- if(!album) album = 'album1';
- pictures.find({album : album}).toArray(function(err,albums){
- console.log(albums);
- console.log("picture:"+albums[0].pictures);
- response.writeHead({"Content-Type":"text/html"});
- var body = "<html><body>";
- var images = "";
- var pictures = albums[0].pictures; 
-pictures.forEach(function(pic){
-  //var imageStr = JSON.stringify(pic.filename);
-  var i = "<img src=data:image/*;base64,"+pic.base64Img+" /><br>";
-  images = i + images ;
- });
- body = body+images+"</body></html>";
- response.end(body);
- });
+ var query = url.parse(request.url,true).query;
+ //console.log(query);
+ //console.log(querystring.parse(request.url));
+ var album = query["album"];
+ var filename = query["filename"];
+ console.log(album);
+ if(!album || !filename) {
+   console.log("album :"+album+"\n"+"filename:"+filename);
+   response.writeHead(400,{"Content-Type":"application/json"}); 
+   response.end();
+ }
+ else{
+        pictures.find({album : album}).toArray(function(err,albums){
+ 		console.log(err)
+                console.log(albums);
+        	console.log("picture:"+albums[0].pictures);
+       	        response.writeHead({"Content-Type":"text/html"});
+       	        var body = "<html><body>";
+      	        var images = "";
+ 		var pictures = albums[0].pictures;
+ 		var flag = ''; 
+ 		pictures.forEach(function(pic){
+  			//var imageStr = JSON.stringify(pic.filename);
+  			if(pic.filename == filename){
+  				flag = 'x';
+  				var i = "<img src=data:image/*;base64,"+pic.base64Img+" /><br>";
+  				images = i + images ;
+  			}
+		});
+ 		if(flag == ''){
+ 			response.writeHead(404,{"Content-Type":"text/html"});
+ 			response.end();
+	 	}
+ 		else{
+ 			response.writeHead(200,{"Content-Type":"text/html"});
+ 			body = body+images+"<body></html>";
+ 			response.end();
+ 		} 
+        });
+ }
 }
 
 exports.start = start;
